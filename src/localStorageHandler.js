f49bee32643a396e8a17de75d1b24d3bc208fda3
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-08-01 20:26:02 -0700

    Fix issue with retreiveLocalStorage not returning projectList

--- src/localStorageHandler.js
+++ src/localStorageHandler.js
@@ -4,6 +4,7 @@ export function retrieveProjectList() {
   if (localStorage.getItem("projectList") === null) {
     const projectList = [];
     projectList.push(createDefaultProject());
+    return projectList;
   } else {
     const projectList = JSON.parse(localStorage.getItem("projectList"));
     return projectList;
