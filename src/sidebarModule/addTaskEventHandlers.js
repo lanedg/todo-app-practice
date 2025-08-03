cbc3bace711b38d64d4b56ccc05e0b600a962fa1
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-08-03 15:15:45 -0700

    Add event handlers associated with addTask button

---
+++ src/sidebarModule/addTaskEventHandlers.js
@@ -0,0 +1,36 @@
+import { addTask } from "../projectModule/project";
+
+export function createAddTaskEventHandlers(projectList) {
+  const addTaskButton = document.getElementById("add-task");
+  const addTaskDialog = document.getElementById("add-task-dialog");
+  const addTaskForm = document.getElementById("add-task-form");
+  const taskDescriptionInput = document.getElementById("task-description");
+  const taskDueDateInput = document.getElementById("task-due-date");
+  const taskCancelButton = document.getElementById("add-task-cancel");
+  const taskSubmitButton = document.getElementById("add-task-submit");
+
+  addTaskButton.addEventListener("click", (e) => {
+    addTaskDialog.showModal();
+  });
+
+  taskCancelButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    addTaskDialog.close();
+    addTaskForm.reset();
+  });
+
+  addTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(data.get("projectSelection"));
+    const projectId = data.get("projectSelection");
+    const description = data.get("taskDescription");
+    const dueDate = data.get("taskDueDate");
+
+    const project = projectList.find((project) => project.id === projectId);
+    addTask(description, dueDate, project);
+
+    addTaskDialog.close();
+    addTaskForm.reset();
+  });
+}
