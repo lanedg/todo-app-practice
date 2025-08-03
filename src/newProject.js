af09e9f641bbc12c976e3c2974dc56fe846bd9fe
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-08-02 20:59:28 -0700

    Add auto storage when creating a project

--- src/newProject.js
+++ src/newProject.js
@@ -1,6 +1,8 @@
 import { Project } from "./project";
+import { storeProjectList } from "./localStorageHandler";
 
 export function createNewProject(name, projectList) {
   const newProject = new Project(name);
   projectList.push(newProject);
+  storeProjectList(projectList);
 }
