343852899f39d1fdfe472be6b96a4c8013cc2e4f
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-08-03 13:49:12 -0700

    Add a way to create the projectlist sidebar elements

---
+++ src/sidebarModule/sidebarManipulator.js
@@ -0,0 +1,44 @@
+export function sidebarProjectListBuilder(projectList) {
+  const sidebarProjectListContainer = document.getElementById(
+    "project-list-container"
+  );
+  projectList.forEach((project) => {
+    const projectContainer = buildProjectContainer(
+      project,
+      sidebarProjectListContainer
+    );
+    buildProjectHeader(project, projectContainer);
+    buildProjectTasks(project, projectContainer);
+  });
+}
+
+function buildProjectContainer(project, projectListContainer) {
+  const container = document.createElement("div");
+  container.classList.add("sidebar-project-container");
+  container.setAttribute("data-projectId", project.id);
+  projectListContainer.appendChild(container);
+  return container;
+}
+
+function buildProjectHeader(project, projectContainer) {
+  const projectHeader = document.createElement("button");
+  projectHeader.classList.add("sidebar-project-button");
+  projectHeader.setAttribute("data-projectId", project.id);
+  projectHeader.textContent = project.name;
+  projectContainer.appendChild(projectHeader);
+}
+
+function buildProjectTasks(project, projectContainer) {
+  project.taskList.forEach((task) => {
+    buildIndividualTask(task, projectContainer);
+  });
+}
+
+function buildIndividualTask(task, projectContainer) {
+  const taskButton = document.createElement("button");
+  taskButton.classList.add("sidebar-task-button");
+  taskButton.setAttribute("data-taskId", task.id);
+  taskButton.setAttribute("data-projectId", projectContainer.dataset.projectId);
+  taskButton.textContent = task.description;
+  projectContainer.appendChild(taskButton);
+}
